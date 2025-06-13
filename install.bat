3445c92a9a94d1fa8cda8fdfa377a4e94bd06a05
impatient13 <105931874+impatient13@users.noreply.github.com>
2025-06-13 20:27:54 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,13 +1,7 @@
 @echo off
 color 6
 
-echo.
-echo ====== Installation des dependances ======
-echo.
-
-:: Installation des modules requis
 python -m pip install requests colorama
 
 echo.
-echo Installation terminee.
 pause
